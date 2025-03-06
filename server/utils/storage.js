ad5a649990def2af1f5acaf3a395e6143d28ff9c
safdaraliniazi <787alisniazi787@gmail.com>
2025-03-06 23:10:09 +0530

    file

--- server/utils/storage.js
+++ server/utils/storage.js
@@ -1,14 +1,39 @@
+// const multer = require('multer');
+
+// // Set up Multer storage options
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads/'); // Destination folder
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${file.originalname}`); // Filename with timestamp
+//     }
+// });
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
+
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+// Ensure uploads directory exists
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Set up Multer storage options
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Destination folder
+        cb(null, uploadDir); // Destination folder
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`); // Filename with timestamp
     }
 });
+
 const upload = multer({ storage });
 
 module.exports = upload;
